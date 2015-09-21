996cba8596a4ed47018a3fd35bad1f85121e06fd
herrfeder <d.lassig@t-online.de>
2015-09-21 20:06:23 +0200

    Add CSRF and changes to SendingFromRobot

--- OWASP_Top_Ten/SensDataExp/SensDataFromRobotTxt.py
+++ OWASP_Top_Ten/SensDataExp/SensDataFromRobotTxt.py
@@ -2,15 +2,16 @@ from threading import Thread, Lock
 from Queue import Queue, Empty
 from mechanize import Browser
 from bs4 import BeautifulSoup as BSoup
+import sys
+import re
 
 gLock = Lock()
-queue = Queue()
+queue = Queue(10)
 threads = []
 class ScapeFromRobots(Thread):
-	url = "http://192.168.8.103/mutillidae/"
+	url = "http://127.0.0.1/mutillidae/"
 	robot = "robots.txt"
 	threadrange = 5
-
 	def __init__(self,queue,br):
 		Thread.__init__(self)
 	
@@ -18,36 +19,37 @@ class ScapeFromRobots(Thread):
 		self.br    = br
 
 	def run(self):
-
+		
 		url = ScapeFromRobots.url
-
+					
 		while True:
-			dir = None
+			dire = None
 
 			try:
-				dir = self.queue.get(timeout=.3)
-
+				dire = self.queue.get(timeout=.3)
 				try:
 					gLock.acquire()
 					self.br.set_handle_robots(False)
-					self.br.open(url.rstrip("/")+ dir)
-
+					self.br.open(url.rstrip("/")+ dire)
+					print "open Site %s"%(url.rstrip("/")+dire)
 					soup = BSoup(self.br.response().read(),'lxml')
+					print soup
 					gLock.release()
 
 					if "Object Not Found" in soup.get_text():
-						print "\t### %s doesn't exist or isn't accessible ###\n"%dir
+						print "\t### %s doesn't exist or isn't accessible ###\n"%dire
 					else:
-						print "\t### Content of %s ###\n"%dir
+						print "\t### Content of %s ###\n"%dire
 						for line in soup.get_text():
 							print line
+		
+					queue.task_done()
 					
-				except Exception:
-					raise
-					print "fertig"
+				except Exception,e:
+					print "Problem occured %s"%e
+					sys.exit(0)
 
 			except Empty:
-				print "fertig"
 				return
 
 	def getRobots(self):
@@ -55,18 +57,19 @@ class ScapeFromRobots(Thread):
 		robotdirs = []
 		url = ScapeFromRobots.url
 		robot = ScapeFromRobots.robot
-
+		print "\t### Search for robots.txt in %s ###"%(url+robot)
 		self.br.open(url+robot)
 
 		soup = BSoup(self.br.response().read(),'lxml')
-		
-		for line in soup.p:
+		stri = soup.p.string
+		dir_list = stri.strip("\n")
+		for line in dir_list:
 
 			if "Disallow: " in line:
 
 				robotdirs.append(line.replace("Disallow: ",""))
 
-			elif "/" in line:
+			elif "/" in line and not "Disallow: " in line:
 				print "\t### Additional Directories found: %s ###"%line
 
 		return robotdirs
@@ -74,20 +77,32 @@ class ScapeFromRobots(Thread):
 
 
 def main():
-
-	try:
+	
+	a = raw_input("Press ENTER to start")
+	if a == "exit":
+		sys.exit(0)
+	else:	
+		print "Start threadrange"
 		for i in range(ScapeFromRobots.threadrange):
 			worker = ScapeFromRobots(queue,Browser(history=None))
 			worker.setDaemon(True)
 			worker.start()
 			threads.append(worker)
-
+		
 		directories = worker.getRobots()
-		print directories
 		for dirline in directories:
 
+			print "Directory: %s"%dirline
 			dirline = dirline.strip().split('\n')
-			queue.put(dirline[0])
+			
+			try:
+				print "Put Queue"
+				queue.put(dirline[0],block=True,timeout=5)
+			except:
+				gLock.acquire()
+				print "\t### Queue is full ###\n"
+				gLock.release()
+				pass	
 
 		queue.join()
 
@@ -95,13 +110,11 @@ def main():
 
 			item.join()
 
-	except (KeyboardInterrupt,SystemExit):
-		pass
 
-	stri = "Scan Completed"
-	print "\t### %s ###\n"%(len(stri)*"#")
-	print "\t### %s ###\n"%stri
-	print "\t### %s ###\n"%(len(stri)*"#")    	
+		stri = "Scan Completed"
+		print "\t### %s ###\n"%(len(stri)*"#")
+		print "\t### %s ###\n"%stri
+		print "\t### %s ###\n"%(len(stri)*"#")    	
 
 if __name__ == "__main__":
 	main()	
